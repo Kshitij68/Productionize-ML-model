47aca2780a8305cd8a27337e6d50d2edd4c3fc72
Kshitij68 <k.mathur68@gmail.com>
2018-11-22 11:21:34 +0530

    Kshitij | Add Results page, Update Predicts page

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    #return 'Hello, Do you think you would have survived the Titanic Disaster?'
     return render_template('index.html')
 
 
@@ -15,29 +14,33 @@ def train():
     return render_template('train.html')
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict')
 def predict():
-    if request.method == "GET":
-        return render_template('predict.html')
-    else:
-        data = {
-            'PassengerId': 891,
-            'Pclass': 3,
-            'Name': 'Kelly, Mr.James',
-            'Sex':'male',
-            'Age':34.5,
-            'SibSp':0,
-            'Parch':0,
-            'Ticket':'330911',
-            'Fare':7.82,
-            'Cabin':'',
-            'Embarked': 'Q',
-            }
-        data = Titanic.get_probability(data)
-        response = {
-            'prediction': data
-        }
-        return jsonify(response)
+    return render_template('predict.html')
+    # else:
+    #     data = {
+    #         'PassengerId': 891,
+    #         'Pclass': 3,
+    #         'Name': 'Kelly, Mr.James',
+    #         'Sex':'male',
+    #         'Age':34.5,
+    #         'SibSp':0,
+    #         'Parch':0,
+    #         'Ticket':'330911',
+    #         'Fare':7.82,
+    #         'Cabin':'',
+    #         'Embarked': 'Q',
+    #         }
+    #     data = Titanic.get_probability(data)
+    #     response = {
+    #         'prediction': data
+    #     }
+    #     return jsonify(response)
+
+@app.route('/results')
+def results():
+    return 0
+
 
 if __name__ == "__main__":
     app.run("127.0.0.1",debug=True)
